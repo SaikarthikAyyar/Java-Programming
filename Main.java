65591098835f45b4bf4eeb5c9aa269c25041ffc9
SaikarthikAyyar <saikarthik132465@gmail.com>
2024-11-23 06:48:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose an operation:");
+        char operation= scanner.next().charAt(0);
+        System.out.print("Enter the first number:");
+        int number1= scanner.nextInt();
+        System.out.print("Enter the second number:");
+        int number2= scanner.nextInt();
+        switch (operation) {
+             case '+':
+                  System.out.print(number1 + "+" + number2 + "=" + (number1 + number2));
+                  break;
+             case '-':
+                  System.out.print(number1 + "-" + number2 + "=" + (number1 - number2));
+                  break;
+             case '*':
+                  System.out.print(number1 + "*" + number2 + "=" + (number1 * number2));
+                  break;
+             case '/':
+                  System.out.print(number1 + "/" + number2 + "=" + (number1 / number2));
+                  break;
+             default:
+                  System.out.print("Invalid operator!");
+        }
+        scanner.close();
+    }
+}
